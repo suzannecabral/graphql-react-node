743c81c5863452420a73395adb68577c027bba36
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2021-10-08 13:35:33 -0700

    gql-node server in progress

--- gql/server/gql-server.js
+++ gql/server/gql-server.js
@@ -1,12 +1,5 @@
-const express = require("express");
 const { ApolloServer } = require("apollo-server");
-const http = require("http");
-require("dotenv").config();
 
-// express server
-const app = express();
-
-const { ApolloServer } = require("apollo-server");
 require("dotenv").config();
 
 // types: query / mutation / subscription
@@ -36,18 +29,4 @@ apolloServer.listen(process.env.PORT, function () {
 	console.log(
 		`graphql server is ready at http://localhost:${process.env.PORT}`
 	);
-});
-
-// applyMiddleware
-
-// rest endpoint
-app.get("/rest", function (req, res) {
-	res.json({
-		data: "rest endpoint is working",
-	});
-});
-
-// port
-app.listen(process.env.PORT, function () {
-	console.log(`server is ready at http://localhost:${process.env.PORT}`);
 });
